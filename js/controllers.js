3e80fbc22cafc13218c392e888c3fec5d09414a0
stratos-gounidellis <t8130029@dias.aueb.gr>
2016-04-24 02:06:58 +0300

    Initial Commit

---
+++ js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('cameraTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+         
+.controller('challengesCtrl', function($scope) {
+
+})
+   
+.controller('makeTheMonkeyJumpCtrl', function($scope) {
+
+})
+   
+.controller('goalsCtrl', function($scope) {
+
+})
+ 
